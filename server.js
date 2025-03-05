c9d42557758fde83566a976a38295e4e6aeb98b3
LukaSpelberg <luka.spelberg@hva.nl>
2025-03-05 17:23:56 +0100

    inloggen met sessions wekt

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const session = require('express-session')
 const app = express()
 
 app
+  .use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET
+  }))
   .use(express.urlencoded({extended: true})) // middleware to parse form data from incoming HTTP request and add form fields to req.body
   .use("/", express.static('static'))        // Allow server to serve static content such as images, stylesheets, fonts or frontend js from the directory named static
   .set('view engine', 'ejs')                 // Set EJS to be our templating engine
   .set('views', 'view')                      // And tell it the views can be found in the directory named view
 
+  .get('/logout', onLogout)
   .get('/login', onLogin)
   .get('/register', onRegister)
 
@@ -18,14 +24,9 @@ app
 
   .listen(8000)
 
-app
-  .use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET
-  }))
 
 
+  
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
@@ -48,7 +49,7 @@ client.connect()
   })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('register.ejs', {username: req.session.user ? req.session.user.username : null})
 })
 
 // Middleware to handle not found errors - error 404
@@ -72,20 +73,31 @@ function onLogin(req, res) {
   res.render('login.ejs')
 }
 
+function onLogout(req, res) {
+  req.session.destroy
+  res.redirect('/')
+}
+
+
 async function onSubmitInlog(req, res) {
-  const dataBase = client.db(process.env.DB_NAME)
-  const collection = dataBase.collection(process.env.DB_COLLECTION)
+  try {
+    const dataBase = client.db(process.env.DB_NAME)
+    const collection = dataBase.collection(process.env.DB_COLLECTION)
 
-  const user = await collection.findOne({
-    username: req.body.username,
-    password: req.body.password
-  })
+    const user = await collection.findOne({
+      username: req.body.username,
+      password: req.body.password
+    })
 
-  if (user) {
-    req.session.user = user
-    res.render('test.ejs', { username: user.username })
-  } else {
-    res.render('inlogfout.ejs')
+    if (user) {
+      req.session.user = { username: user.username}
+      res.redirect('/')
+    } else {
+      res.render('inlogfout.ejs')
+    }
+  } catch (error) {
+    console.error('Error during login:', error)
+    res.status(500).send('500: server error')
   }
 }
 
